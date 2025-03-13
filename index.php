2aca89e6d5510d27d6ad4d6f9588ea8b054a362b
JandriTampubolon <jandri.tampubolon@students.amikom.ac.id>
2025-03-13 16:17:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<?php
-header("Location: /root");
-exit;
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  Loading
+</body>
+</html>
